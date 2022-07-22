9ed8bf3db598c18333a78216ab7e4a7c36577306
anokidev <anoki.youssou@gmail.com>
2022-07-22 10:47:33 +0700

    main.js is finished

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
 import Canvas from "render.js";
-import { Game, Player, Fruit } from "game.js";
 import Input from "input.js";
-import Update from "update.js";
 
+import Game from "game.js";
+import Player from "player.js";
+import Fruit from "fruit.js";
  
-class SnakeGame {
+class Main {
 
     constructor(config) {
 
@@ -12,44 +13,71 @@ class SnakeGame {
         this.config = config;
 
         // Technical stuff.
-        this.canvas = new Canvas(this.config.canvasID);
-        this.update = new Update(this.config.renderFPS);
+        this.canvas = new Canvas(this.config.canvasID, this.config.renderFPS);
+        this.input = new Input(this.config.inputSystem);
 
         // Game stuff.
-        this.game = new Game(this.config.backgroundColor);
+        this.game = new Game();
         this.player = new Player(this.config.playerColor, this.config.playerGrowth, playerSpeed)
         this.fruit = new Fruit(this.config.fruitColor);
 
     };
 
+    updateGame() {
+
+        this.player.update();
+        this.fruit.update();
+
+    };
+
+    prepareGame() {
+
+        window.addEventListener("resize", this.canvas.resize());
+        window.addEventListener("keydown", this.input.keyDown());
+        window.addEventListener("keyup", this.input.keyUp());
+
+        // When updating the canvas, call updateGame().
+        this.canvas.update(this.updateGame);
+
+    };
+
+    initGame() {
+
+        // Prepare the game.
+        this.prepareGame();
+
+        // Start the engine.
+        this.canvas.start();
+
+    };
+
 };
-
-// Configs.
-
 
 // Run.
 window.addEventListener('load', (event) => {
 
-    let config = {
+    // Configs.
+    let configs = {
 
-        canvasID : "canvas",
+        canvasID : "canvas",             // Canvas ID.
     
-        backgroundColor : "#000000",
-        fruitColor : "#ff3503",
+        backgroundColor : "#000000",     // Background color.
+        fruitColor : "#ff3503",          // Fruit color.
     
-        playerSpeed : 1,
-        playerColor : "#03ff0b",
-        playerGrowth : "1",
+        playerSpeed : 1,                 // Player movement speed.
+        playerColor : "#03ff0b",         // Player color.
+        playerGrowth : "1",              // Player growth speed.
     
-        renderFPS : "1",
+        renderFPS : "1",                 // FPS.
     
-        inputSystem : "WASD"
+        inputSystem : [87, 65, 83, 68]   // Keybindings. Each number represents a keyboard button.
     
     };
 
     // Load configs.
-    const main = Main(config);
+    const main = Main(configs);
 
-    // Link events.
+    // Run.
+    main.initGame();
 
 });
