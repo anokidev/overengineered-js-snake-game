1b2fa6f37fe8683f04bb623cf276a867c74c2bdf
anokidev <anoki.youssou@gmail.com>
2022-07-22 20:44:31 +0700

    not finished yet

---
+++ src/game.js
@@ -0,0 +1,67 @@
+/*
+
+    ./game/environ.js
+
+    This file handles the game's
+    environment.
+
+    Copyright (c) 2022 Matthew Zhou,
+    all right reserved. Licensed in MIT License.
+
+*/
+
+class Game {
+
+    constructor(config) {
+
+        this.config = config;
+
+    };
+
+    initGame(canvas) {
+
+        // Context.
+        this.context = canvas.newContext(
+            config.background,
+            config.resolution
+        );
+
+        // Player.
+        this.player  = canvas.newGrids(
+            config.startingPosition, 
+            config.startingLength,
+            config.startingPosition
+        );
+
+    };
+
+    keyPressHandler(event) {
+
+        // Check the keyPress event. The one I typed in main.js is "WASD". 
+
+        // Up.
+        if (event.keyCode == this.inputSystem[0]) {
+            this.player.updateDirection("up");
+            this.player.updateGrids("add", config.growthSpeed);
+
+        // Left.
+        } else if (event.keyCode == this.inputSystem[1]) {
+            this.player.updateDirection("left");
+            this.player.updateGrids("add", config.growthSpeed);
+
+        // Down.
+        } else if (event.keyCode == this.inputSystem[2]) {
+            this.player.updateDirection("down");
+            this.player.updateGrids("add", config.growthSpeed);
+
+        // Right.
+        } else if (event.keyCode == this.inputSystem[3]) {
+            this.player.updateDirection("right");
+            this.player.updateGrids("add", config.growthSpeed);
+
+        // Other button.
+        } else {};
+
+    };
+
+};
---
+++ src/main.js
@@ -0,0 +1,96 @@
+/*
+
+    ./main.js
+
+    Entrypoint. This is going to manage
+    the whole game process.
+
+    Copyright (c) 2022 Matthew Zhou,
+    all right reserved. Licensed in MIT License.
+
+*/
+
+import Canvas from "render.js";
+import Game from "environ.js";
+ 
+class Main {
+
+    constructor(config) {
+
+        // Config.
+        this.config = config;
+
+        // Canvas.
+        this.canvas = new Canvas(this.config.canvas);
+
+        // Game.
+        this.game = new Game(this.config.game);
+
+    };
+
+    updateGame() {
+
+        // Update the game state.
+        this.game.update();
+
+    };
+
+    prepareGame() {
+
+        // Event listener.
+        window.addEventListener("resize", this.canvas.resize);
+        window.addEventListener("keyup", this.input.keyPress);
+
+        // When updating the canvas, call updateGame().
+        this.canvas.update(this.updateGame);
+
+        // Load background.
+        this.canvas.loadBackground(this.config.backgroundColor);
+
+    };
+
+    initGame() {
+
+        // Prepare the game.
+        this.prepareGame();
+
+        // Start the engine.
+        this.canvas.start();
+
+    };
+
+};
+
+// Run.
+window.addEventListener('load', (event) => {
+
+    // Configs.
+    let configs = {
+
+        canvas : {
+            canvasID          : "canvas",            // Canvas ID.
+            renderFPS         : "1000 / 30"          // Render FPS speed.
+        },
+
+        game : {
+            background        : "#000000",           // Background color.
+            fruit             : "#fff3503",          // Fruit color.
+            startingLength    : 2,                   // Player length,
+            color             : "#03ff0b",           // Player color.
+            growthSpeed       : 1,                   // Player growth speed.
+            movementSpeed     : 1,                   // Player movement speed.
+            keybindings       : [119, 97, 115, 100], // Input keybindings.
+            startingPosition  : [0, 0],              // Starting position.
+            startingDirection : "right",             // Starting direction.
+            resolution        : "fitxfit"            // Resolution.
+        }
+
+    };
+
+    // Load configs.
+    const main = Main(configs);
+
+    // Run.
+    main.initGame();
+
+});
